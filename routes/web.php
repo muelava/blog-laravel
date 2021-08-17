7510fb9a1b7ea5e9db755abe815fd47c82d3c49b
unknown <muhammadelanghardifal>
2021-08-17 12:17:14 +0700

    pertemuan 15 user registration

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
-use App\Models\User;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -47,3 +47,9 @@ Route::get('/categories', function () {
         'categories' => Category::all()
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
+
+
+Route::post('/register', [RegisterController::class, 'store']);
